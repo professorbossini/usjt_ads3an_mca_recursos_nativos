823ed78d3b14fdbcd6c0489fe4bdfe2b75264d6f
Rodrigo Bossini <professorbossini@gmail.com>
2020-05-18 21:07:18 -0300

    Terminamos o pdf 10.

--- App.js
+++ App.js
@@ -4,6 +4,17 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reduxThunk from 'redux-thunk'
 import lugaresReducer from './store/lugares-reducer';
+import { init } from './helpers/db';
+
+init().
+  then(() => {
+    console.log("Base criada com sucesso ou já existente.");
+  }).
+  catch((err) => {
+    console.log("Criação da base falhou.");
+    console.log(err);
+  })
+
 
 const rootReducer = combineReducers({
   lugares: lugaresReducer
--- store/lugares-actions.js
+++ store/lugares-actions.js
@@ -1,7 +1,47 @@
+import * as FileSystem from 'expo-file-system'
+import { inserirLugar, buscarLugares } from '../helpers/db';
+
 export const ADD_LUGAR = 'ADD_LUGAR';
+export const LISTA_LUGARES = "LISTA_LUGARES";
+
+export const listarLugares = () => {
+    return async dispatch => {
+        try {
+            const resultadoDB = await buscarLugares();
+            dispatch({ type: LISTA_LUGARES, lugares: resultadoDB.rows._array })
+        }
+        catch (err) {
+            console.log(err);
+            throw err;
+        }
+    }
+}
+
 
 export const addLugar = (nomeLugar, imagem) => {
-    return {
-        type: ADD_LUGAR, dadosLugar: { nomeLugar: nomeLugar, imagem: imagem }
+    return async dispatch => {
+        //file://diretorio/outrodiretorio/nome.png
+        const nomeArquivo = imagem.split("/").pop();
+        const novoPath = FileSystem.documentDirectory + nomeArquivo;
+        try {
+            await FileSystem.moveAsync({
+                from: imagem,
+                to: novoPath
+            })
+            const resultadoDB = await inserirLugar(
+                nomeLugar,
+                novoPath,
+                'Torre Eiffel',
+                48.8584,
+                2.2945
+            )
+            //console.log(resultadoDB);
+            dispatch({ type: ADD_LUGAR, dadosLugar: { id: resultadoDB.insertId, nomeLugar: nomeLugar, imagem: novoPath } })
+        }
+        catch (err) {
+            console.log(err);
+            throw err;
+        }
     }
+
 }
